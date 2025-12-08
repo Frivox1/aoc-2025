13d2016e2a69fffb32f7be179ba291034d20cc05
Frivox1 <mertens.valery@gmail.com>
2025-12-08 21:57:43 +0100

    day-3 part2

--- day-3/part2.py
+++ day-3/part2.py
@@ -1,16 +1,33 @@
 input = []
 answer = 0
 
-with open("small.txt") as f:
+with open("input.txt") as f:
     for line in f:
         input.append(line.strip())
 
 
 
 for line in input:
-    digits = [(int(line[i]), i) for i in range(len(line))]
-    digits.sort(reverse=True, key=lambda x: x[0])
-    print("digits:", digits)
+    stack = []
+    length = 12
+    for i in range(len(line)):
+        c = line[i]
+        print("Considering:", c, "at", i)
+        while (len(stack) + (len(line) - i) > length and
+               len(stack) > 0 and
+               stack[-1] < c):
+            print("  Popping:", stack[-1])
+            stack.pop()
+        if len(stack) < length:
+            stack.append(c)
+            print("  Pushing:", c)
+        print("  Stack now:", stack)
+    number = ''.join(stack)
+    answer += int(number)
+
+
     
 
 print("Answer:", answer)
+
+print(answer == 3121910778619)
